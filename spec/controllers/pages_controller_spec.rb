5203b7e199dc5ce502db5f445d7548be706b187f
ToanDK <khactoan96@gmail.com>
2017-12-28 14:04:25 +0700

    Finish fix show user page after user signed in and overide rspec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,13 +1,36 @@
 require "rails_helper"
 
 RSpec.describe PagesController, type: :controller do
-  describe "GET #index" do
-    before do
-      get :index
+  describe "#index" do
+    let(:user){FactoryBot.create :user}
+
+    context "user has already signed in" do
+      before do
+        sign_in user
+        get :index
+      end
+
+      it "return a 302 response" do
+        expect(response).to have_http_status "302"
+      end
+
+      it "redirect to user page" do
+        expect(response).to redirect_to user_path user
+      end
     end
 
-    it "responds successfully with an HTTP 200 status code" do
-      expect(response).to be_success
+    context "user has not signed in yet" do
+      before do
+        get :index
+      end
+
+      it "responds successfully" do
+        expect(response).to be_success
+      end
+
+      it "returns a 200 response" do
+        expect(response).to have_http_status  "200"
+      end
     end
   end
 end
